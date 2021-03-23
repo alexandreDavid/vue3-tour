579b914834d97b69dd5325474da0016b01ef7e41
Alexandre David <alexandre.David42@gmail.com>
2021-03-23 19:40:01 +0100

    Fix pipeline

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,20 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // build: {
-  //   lib: {
-  //     entry: path.resolve(__dirname, 'src/main.js'),
-  //     name: 'Vue3Tour'
-  //   },
-  //   rollupOptions: {
-  //     external: ['vue'],
-  //     output: {
-  //       assetFileNames: `vue3-tour.[ext]`,
-  //       globals: {
-  //         vue: 'Vue'
-  //       }
-  //     }
-  //   }
-  // },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.js'),
+      name: 'Vue3Tour'
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        assetFileNames: `vue3-tour.[ext]`,
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
   plugins: [vue()]
 })
