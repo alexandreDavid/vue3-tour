ee55a0715c750cab9fad70478a6ca489de6f766c
Alexandre David <alexandre.David42@gmail.com>
2022-09-05 22:22:27 +0200

    fix: popper fix upgrade

--- src/components/VStep.vue
+++ src/components/VStep.vue
@@ -3,31 +3,35 @@
     <!-- <div class="tour-mask" v-show="displayMask">
       <div class="tour-focus-container" v-bind:style="styleFocusContainer"></div>
     </div> -->
-    <div class="v-step" :id="'v-step-' + hash" ref="VStep">
-      <slot name="header">
-        <div v-if="step.header" class="v-step__header">
-          <div v-if="step.header.title" v-html="step.header.title"></div>
-        </div>
-      </slot>
+  <div :class="{ 'v-step--sticky': isSticky }" class="v-step" :id="'v-step-' + hash" ref="VStep">
+    <slot name="header">
+      <div v-if="step.header" class="v-step__header">
+        <div v-if="step.header.title" v-html="step.header.title"></div>
+      </div>
+    </slot>
 
-      <slot name="content">
-        <div class="v-step__content">
-          <div v-if="step.content" v-html="step.content"></div>
-          <div v-else>props is a demo step! The id of props step is {{ hash }} and it targets {{ step.target }}.</div>
-        </div>
-      </slot>
+    <slot name="content">
+      <div class="v-step__content">
+        <div v-if="step.content" v-html="step.content"></div>
+        <div v-else>props is a demo step! The id of props step is {{ hash }} and it targets {{ step.target }}.</div>
+      </div>
+    </slot>
 
-      <slot name="actions">
-        <div class="v-step__buttons">
-          <button @click.prevent="skip" v-if="!isLast && isButtonEnabled('buttonSkip')" class="v-step__button v-step__button-skip">{{ labels.buttonSkip }}</button>
-          <button @click.prevent="previousStep" v-if="!isFirst && isButtonEnabled('buttonPrevious')" class="v-step__button v-step__button-previous">{{ labels.buttonPrevious }}</button>
-          <button @click.prevent="nextStep" v-if="!isLast && isButtonEnabled('buttonNext')" class="v-step__button v-step__button-next">{{ labels.buttonNext }}</button>
-          <button @click.prevent="finish" v-if="isLast && isButtonEnabled('buttonStop')" class="v-step__button v-step__button-stop">{{ labels.buttonStop }}</button>
-        </div>
-      </slot>
+    <slot name="actions">
+      <div class="v-step__buttons">
+        <button @click.prevent="skip" v-if="!isLast && isButtonEnabled('buttonSkip')"
+          class="v-step__button v-step__button-skip">{{ labels.buttonSkip }}</button>
+        <button @click.prevent="previousStep" v-if="!isFirst && isButtonEnabled('buttonPrevious')"
+          class="v-step__button v-step__button-previous">{{ labels.buttonPrevious }}</button>
+        <button @click.prevent="nextStep" v-if="!isLast && isButtonEnabled('buttonNext')"
+          class="v-step__button v-step__button-next">{{ labels.buttonNext }}</button>
+        <button @click.prevent="finish" v-if="isLast && isButtonEnabled('buttonStop')"
+          class="v-step__button v-step__button-stop">{{ labels.buttonStop }}</button>
+      </div>
+    </slot>
 
-      <div class="v-step__arrow" :class="{ 'v-step__arrow--dark': step.header && step.header.title }"></div>
-    </div>
+    <div class="v-step__arrow" :class="{ 'v-step__arrow--dark': step.header && step.header.title }"></div>
+  </div>
   <!-- </teleport> -->
 </template>
 
@@ -105,6 +109,9 @@ export default {
       }
     })
 
+    const isSticky = computed(() => {
+      return !props.step.target
+    })
 
     const VStep = ref(null)
 
@@ -113,24 +120,28 @@ export default {
         console.log('[Vue Tour] The target element ' + props.step.target + ' of .v-step[id="' + hash + '"] is:', targetElement)
       }
 
-      if (targetElement) {
-        enableScrolling()
-        createHighlight()
-
-        createPopper(
-          targetElement,
-          VStep.value,
-          params.value
-        )
+      if (isSticky.value) {
+        document.body.appendChild(VStep)
       } else {
-        if (props.debug) {
-          console.error('[Vue Tour] The target element ' + props.step.target + ' of .v-step[id="' + hash + '"] does not exist!')
+        if (targetElement) {
+          enableScrolling()
+          createHighlight()
+
+          createPopper(
+            targetElement,
+            VStep.value,
+            params.value
+          )
+        } else {
+          if (props.debug) {
+            console.error('[Vue Tour] The target element ' + props.step.target + ' of .v-step[id="' + hash + '"] does not exist!')
+          }
+          context.emit('targetNotFound', props.step)
+          if (props.stopOnFail) {
+            props.stop()
+          }
         }
-        context.emit('targetNotFound', props.step)
-        if (props.stopOnFail) {
-          props.stop()
-        }
-      }
+    }
     }
 
     const enableScrolling = () => {
@@ -200,157 +211,140 @@ export default {
 
     onUnmounted(removeHighlight)
 
-    return { hash, isButtonEnabled, VStep }
+    return { hash, isButtonEnabled, VStep, isSticky }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .v-step {
-    background: #50596c; /* #ffc107, #35495e */
-    color: white;
-    max-width: 320px;
-    border-radius: 3px;
-    filter: drop-shadow(0 0 2px rgba(0, 0, 0, 0.5));
-    padding: 1rem;
-    text-align: center;
-    z-index: 10000;
-  }
+.v-step {
+  background: #50596c;
+  /* #ffc107, #35495e */
+  color: white;
+  max-width: 320px;
+  border-radius: 3px;
+  box-shadow: rgba(0, 0, 0, 0) 0px 0px 0px 0px,
+    rgba(0, 0, 0, 0) 0px 0px 0px 0px,
+    rgba(0, 0, 0, 0.1) 0px 4px 6px -1px,
+    rgba(0, 0, 0, 0.06) 0px 2px 4px -1px;
+  padding: 10px;
+  pointer-events: auto;
+  text-align: center;
+  z-index: 10000;
 
-  .v-step .v-step__arrow {
-    width: 0;
-    height: 0;
-    border-style: solid;
+  &--sticky {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    & .v-step__arrow {
+      display: none;
+    }
+  }
+}
+
+.v-step__arrow,
+.v-step__arrow::before {
+  position: absolute;
+  width: 10px;
+  height: 10px;
+  background: inherit;
+}
+
+.v-step__arrow {
+  visibility: hidden;
+
+  &--dark {
+    &:before {
+      background: #454d5d;
+    }
+  }
+}
+
+.v-step__arrow::before {
+  visibility: visible;
+  content: '';
+  transform: rotate(45deg);
+}
+
+.v-step[data-popper-placement^="top"]>.v-step__arrow {
+  bottom: -5px;
+  left: calc(50% - 10px);
+}
+
+.v-step[data-popper-placement^="bottom"]>.v-step__arrow {
+  top: -5px;
+  left: calc(50% - 10px);
+}
+
+.v-step[data-popper-placement^="right"]>.v-step__arrow {
+  left: -5px;
+  top: calc(50% - 10px);
+}
+
+.v-step[data-popper-placement^="left"]>.v-step__arrow {
+  right: -5px;
+  top: calc(50% - 10px);
+}
+
+/* Custom */
+.v-step__header {
+  margin: -10px -10px 5px;
+  padding: 5px;
+  background-color: #454d5d;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+}
+
+.v-step__content {
+  margin: 0 0 10px 0;
+}
+
+.v-step__button {
+  background: transparent;
+  border: .05rem solid white;
+  border-radius: .10px;
+  color: white;
+  cursor: pointer;
+  display: inline-block;
+  font-size: .8rem;
+  height: 1.8rem;
+  line-height: 10px;
+  outline: none;
+  margin: 0 0.2rem;
+  padding: .35rem .4rem;
+  text-align: center;
+  text-decoration: none;
+  transition: all .2s ease;
+  vertical-align: middle;
+  white-space: nowrap;
+
+  &:hover {
+    background-color: rgba(white, 0.95);
+    color: #50596c;
+  }
+}
+
+.mask {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: rgba(0, 0, 0, .5);
+
+  .tour-focus-container {
+    border-radius: 4px;
+    transition: opacity 0.2s;
+    left: 0px;
+    top: 0px;
+    width: 100%;
+    height: 100%;
     position: absolute;
-    margin: 0.5rem;
+    opacity: 1;
+    pointer-events: auto;
+    box-shadow: 0px 0px 0px 9999px rgba(17, 55, 80, 0.4), 0px 0px 15px rgba(0, 0, 0, 0.5);
   }
-
-  .v-step .v-step__arrow {
-    border-color: #50596c; /* #ffc107, #35495e */
-
-    &--dark {
-      border-color: #454d5d;
-    }
-  }
-
-  .v-step[x-placement^="top"] {
-    margin-bottom: 0.5rem;
-  }
-
-  .v-step[x-placement^="top"] .v-step__arrow {
-    border-width: 0.5rem 0.5rem 0 0.5rem;
-    border-left-color: transparent;
-    border-right-color: transparent;
-    border-bottom-color: transparent;
-    bottom: -0.5rem;
-    left: calc(50% - 1rem);
-    margin-top: 0;
-    margin-bottom: 0;
-  }
-
-  .v-step[x-placement^="bottom"] {
-    margin-top: 0.5rem;
-  }
-
-  .v-step[x-placement^="bottom"] .v-step__arrow {
-    border-width: 0 0.5rem 0.5rem 0.5rem;
-    border-left-color: transparent;
-    border-right-color: transparent;
-    border-top-color: transparent;
-    top: -0.5rem;
-    left: calc(50% - 1rem);
-    margin-top: 0;
-    margin-bottom: 0;
-  }
-
-  .v-step[x-placement^="right"] {
-    margin-left: 0.5rem;
-  }
-
-  .v-step[x-placement^="right"] .v-step__arrow {
-    border-width: 0.5rem 0.5rem 0.5rem 0;
-    border-left-color: transparent;
-    border-top-color: transparent;
-    border-bottom-color: transparent;
-    left: -0.5rem;
-    top: calc(50% - 1rem);
-    margin-left: 0;
-    margin-right: 0;
-  }
-
-  .v-step[x-placement^="left"] {
-    margin-right: 0.5rem;
-  }
-
-  .v-step[x-placement^="left"] .v-step__arrow {
-    border-width: 0.5rem 0 0.5rem 0.5rem;
-    border-top-color: transparent;
-    border-right-color: transparent;
-    border-bottom-color: transparent;
-    right: -0.5rem;
-    top: calc(50% - 1rem);
-    margin-left: 0;
-    margin-right: 0;
-  }
-
-  /* Custom */
-
-  .v-step__header {
-    margin: -1rem -1rem 0.5rem;
-    padding: 0.5rem;
-    background-color: #454d5d;
-    border-top-left-radius: 3px;
-    border-top-right-radius: 3px;
-  }
-
-  .v-step__content {
-    margin: 0 0 1rem 0;
-  }
-
-  .v-step__button {
-    background: transparent;
-    border: .05rem solid white;
-    border-radius: .1rem;
-    color: white;
-    cursor: pointer;
-    display: inline-block;
-    font-size: .8rem;
-    height: 1.8rem;
-    line-height: 1rem;
-    outline: none;
-    margin: 0 0.2rem;
-    padding: .35rem .4rem;
-    text-align: center;
-    text-decoration: none;
-    transition: all .2s ease;
-    vertical-align: middle;
-    white-space: nowrap;
-
-    &:hover {
-      background-color: rgba(white, 0.95);
-      color: #50596c;
-    }
-  }
-
-  .mask {
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    background: rgba(0,0,0,.5);
-
-    .tour-focus-container {
-      border-radius: 4px;
-      transition:opacity 0.2s;
-      left: 0px;
-      top: 0px;
-      width: 100%;
-      height: 100%;
-      position: absolute;
-      opacity: 1;
-      pointer-events: auto;
-      box-shadow: 0px 0px 0px 9999px rgba(17,55,80,0.4), 0px 0px 15px rgba(0,0,0,0.5);
-    }
-  }
+}
 </style>
--- src/shared/constants.js
+++ src/shared/constants.js
@@ -47,11 +47,20 @@ export const DEFAULT_STEP_OPTIONS = {
   enableScrolling: true,
   highlight: DEFAULT_OPTIONS.highlight, // By default use the global tour setting
   enabledButtons: DEFAULT_OPTIONS.enabledButtons,
-  modifiers: {
-    arrow: {
-      element: '.v-step__arrow'
-    }
-  },
+  modifiers: [
+    {
+      name: 'offset',
+      options: {
+        offset: ({ placement }) => {
+          if (placement === 'top' || placement === 'bottom') {
+            return [0, 8];
+          } else {
+            return [8, 0];
+          }
+        },
+      },
+    },
+  ],
   placement: 'bottom'
 }
 
